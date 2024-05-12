a932c789851ff0f550e50c27bda81d57b5c9b004
Shengyou Fan <shengyoufan@gmail.com>
2024-05-12 18:59:10 +0800

    🏆 4-3: Write More Tests

--- test_main.py
+++ test_main.py
@@ -9,3 +9,23 @@ class TestRootRoute:
         response = client.get("/")
         assert response.status_code == 200
         assert response.json() == {"message": "Hello FastAPI"}
+
+
+class TestHiRoute:
+
+    def test_string(self):
+        name = "Jane"
+        response = client.get("/hi/%s" % name)
+        assert response.status_code == 200
+        assert response.json() == {"message": ("Hi %s" % name)}
+
+    def test_empty(self):
+        name = ""
+        response = client.get("/hi/%s" % name)
+        assert response.status_code == 404
+
+    def test_non_alpha(self):
+        name = "123"
+        response = client.get("/hi/%s" % name)
+        assert response.status_code == 200
+        assert response.json() == {"message": ("Hi %s" % name)}
